1787a7c45f9fb3d7f7098cb2710f8c9abd41bee7
tanyun <2879823308@qq.com>
2020-03-14 21:09:14 +0800

    修改FirstClass

--- src/main/java/com/git/FirstClass.java
+++ src/main/java/com/git/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        // hello test
     }
 
 
@@ -14,5 +15,6 @@ public class FirstClass {
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
